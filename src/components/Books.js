ed1d20e739584e16fbeb56576f7ef5c31c30a30a
Murat Sercan <muratsercann@gmail.com>
2024-07-26 22:20:45 +0300

    ..

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,57 +1,208 @@
-import { Table } from "react-bootstrap";
+import React, { useState, useMemo } from "react";
+import { Table, Form, Row, Col } from "react-bootstrap";
 
 export default function Books({ products }) {
-  let totalStr = "";
+  const [selectedAuthor, setSelectedAuthor] = useState("");
+  const [selectedPublisher, setSelectedPublisher] = useState("");
+  const [sortOption, setSortOption] = useState("recommended"); // Varsayılan sıralama
+
+  const handleClearFilters = (event) => {
+    event.preventDefault(); // Linkin varsayılan davranışını engelle
+
+    setSelectedAuthor(""); // Yazar filtresini temizle
+    setSelectedPublisher(""); // Yayıncı filtresini temizle
+    setSortOption("recommended"); // Varsayılan sıralama seçeneğine döndür
+
+    // Ürünleri yeniden göster (opsiyonel)
+    // setProducts(originalProducts); // Eğer filtrelenmiş ürünleri saklıyorsanız
+  };
+  // Filtreleme ve sıralama fonksiyonu
+  const filteredAndSortedProducts = useMemo(() => {
+    // Filtreleme
+    const filtered = products.filter((product) => {
+      const matchesAuthor = selectedAuthor
+        ? product.writer.includes(selectedAuthor)
+        : true;
+      const matchesPublisher = selectedPublisher
+        ? product.publisher.includes(selectedPublisher)
+        : true;
+      return matchesAuthor && matchesPublisher;
+    });
+
+    const parsePrice = (priceString) => {
+      const price = priceString
+        .replaceAll(" ", "")
+        .replaceAll("TL", "")
+        .replaceAll(".", "")
+        .replaceAll(",", ".");
+
+      const result = parseFloat(price);
+      return result;
+    };
+
+    // Sıralama
+    const sorted = [...filtered].sort((a, b) => {
+      const priceA = parsePrice(a.price);
+      const priceB = parsePrice(b.price);
+
+      switch (sortOption) {
+        case "ascending":
+          return priceA - priceB;
+        case "descending":
+          return priceB - priceA;
+        default: // recommended (varsayılan sıralama)
+          return 0; // Varsayılan sıralama, değişiklik yapmaz
+      }
+    });
+
+    return sorted;
+  }, [products, selectedAuthor, selectedPublisher, sortOption]);
+
+  // Yazar ve yayınevi seçeneklerini oluşturma
+
+  let authors = [...new Set(products.map((product) => product.writer))].sort();
+  let publishers = [
+    ...new Set(products.map((product) => product.publisher)),
+  ].sort();
+
+  // Seçilen yazar varsa, sadece bu yazarın bulunduğu yayınevlerini göster
+  publishers =
+    selectedAuthor !== ""
+      ? [
+          ...new Set(
+            products
+              .filter((product) => product.writer === selectedAuthor)
+              .map((product) => product.publisher)
+          ),
+        ].sort()
+      : publishers;
+
+  // Seçilen yayınevi varsa, sadece bu yayınevindeki yazarları göster
+  authors =
+    selectedPublisher !== ""
+      ? [
+          ...new Set(
+            products
+              .filter((product) => product.publisher === selectedPublisher)
+              .map((product) => product.writer)
+          ),
+        ].sort()
+      : authors;
 
   function formatPrice(price) {
     let formattedPrice = price;
 
     if (formattedPrice.indexOf(",") < 0) formattedPrice += ",00";
     return formattedPrice + " TL";
-    // return price.toLocaleString("tr-TR", {
-    //   style: "currency",
-    //   currency: "TRY",
-    //   minimumFractionDigits: 2,
-    //   maximumFractionDigits: 2,
-    // });
   }
 
-  if (products.length > 0) {
-    totalStr = (
+  let totalStr =
+    products.length > 0 ? (
       <span>
-        <strong>{products.length + " books"}</strong> {" found"}
+        <strong>{filteredAndSortedProducts.length + " sonuç bulundu"}</strong>
       </span>
+    ) : (
+      <span>Sonuç bulunamadı</span>
     );
-  } else {
-    totalStr = <span>No books found</span>;
-  }
+
   return (
     <div
       style={{
         display: "flex",
-        flexDirection: "column",
-        justifyItems: "flex-start",
+        flexDirection: "row",
+        justifyContent: "flex-start",
         alignItems: "flex-start",
+        gap: "10px",
       }}
     >
-      <div className="mb-3">{totalStr}</div>
-      {products && products.length > 0 && (
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>No</th>
-              <th>Mağaza</th>
-              <th>Görsel</th>
-              <th>Yayınevi</th>
-              <th>Kitap Adı</th>
-              <th>Yazar</th>
-              <th>Fiyat</th>
-              <th>Link</th>
-            </tr>
-          </thead>
-          <tbody>
-            {products.length > 0 ? (
-              products.map((product, index) => (
+      <div>
+        <div className="mb-3" style={{ width: "200px" }}>
+          <Form.Group controlId="authorFilter">
+            <Form.Label
+              style={{
+                textAlign: "left",
+                display: "block",
+              }}
+            >
+              Yazar :
+            </Form.Label>
+            <Form.Select
+              className="mb-4"
+              value={selectedAuthor}
+              onChange={(e) => setSelectedAuthor(e.target.value)}
+            >
+              <option value="">Tüm Yazarlar</option>
+              {authors.map((author, index) => (
+                <option key={index} value={author}>
+                  {author}
+                </option>
+              ))}
+            </Form.Select>
+          </Form.Group>
+
+          <Form.Group controlId="publisherFilter">
+            <Form.Label
+              style={{
+                textAlign: "left",
+                display: "block",
+              }}
+            >
+              Yayınevi :
+            </Form.Label>
+            <Form.Select
+              className="mb-3"
+              value={selectedPublisher}
+              onChange={(e) => setSelectedPublisher(e.target.value)}
+            >
+              <option value="">Tüm Yayınevleri</option>
+              {publishers.map((publisher, index) => (
+                <option key={index} value={publisher}>
+                  {publisher}
+                </option>
+              ))}
+            </Form.Select>
+          </Form.Group>
+
+          <Form.Group controlId="sortOption">
+            <Form.Label style={{ textAlign: "left", display: "block" }}>
+              Sıralama :
+            </Form.Label>
+            <Form.Select
+              as="select"
+              className="custom-select"
+              value={sortOption}
+              onChange={(e) => setSortOption(e.target.value)}
+            >
+              <option value="recommended">Önerilen</option>
+              <option value="ascending">Artan Fiyat</option>
+              <option value="descending">Azalan Fiyat</option>
+            </Form.Select>
+          </Form.Group>
+        </div>
+        <div className="mb-3">{totalStr}</div>
+
+        <a href="#" onClick={handleClearFilters}>
+          Temizle
+        </a>
+      </div>
+
+      <div>
+        {filteredAndSortedProducts.length > 0 && (
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>No</th>
+                <th>Mağaza</th>
+                <th>Görsel</th>
+                <th>Yayınevi</th>
+                <th>Kitap Adı</th>
+                <th>Yazar</th>
+                <th>Fiyat</th>
+                <th>Link</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredAndSortedProducts.map((product, index) => (
                 <tr key={index}>
                   <td>{index + 1}</td>
                   <td>{product.store}</td>
@@ -72,15 +223,11 @@ export default function Books({ products }) {
                     </a>
                   </td>
                 </tr>
-              ))
-            ) : (
-              <tr>
-                <td colSpan="3">No products found</td>
-              </tr>
-            )}
-          </tbody>
-        </Table>
-      )}
+              ))}
+            </tbody>
+          </Table>
+        )}
+      </div>
     </div>
   );
 }
